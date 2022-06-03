5dceaaa8ff04428272b414c03e55197de8b70878
DELL of HIEP <DELL of HIEP@DESKTOP-K0P039V>
2022-06-03 15:46:34 +0700

    Add project files.

---
+++ LabBigSchool_CuDucHiep/Models/Course.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabBigSchool_CuDucHiep.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+
+    }
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        
+        public string Date { get; set; }
+        [Required]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+
+    }
+
+
+
+}
---
+++ LabBigSchool_CuDucHiep/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_CuDucHiep.Startup))]
+namespace LabBigSchool_CuDucHiep
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
